2b97057b43c02c1ba39137b77e5453351437bc65
MidnightOwll <dv92040@gmail.com>
2021-08-14 15:55:44 +0300

    spa vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+ <nav class="blue darken-3">
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo">Tasks</router-link>
+      <ul class="right hide-on-med-and-down">
+        <router-link             
+            to="/"
+            custom 
+            
+            v-slot="{href, navigate, isExactActive }"            
+        >
+            <li :class="[isExactActive && 'active']"><a :href="href" @click="navigate">Create</a></li>
+        </router-link>
+        <router-link             
+            to="/list"
+            custom            
+            v-slot="{href, navigate, isActive }"            
+        >
+            <li :class="[isActive && 'active']"><a :href="href" @click="navigate">List</a></li>
+        </router-link>
+        
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+    nav {
+      padding: 0 2rem;  
+    }
+</style>
